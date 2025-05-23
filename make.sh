4921fe2e28658f468553539fe0c15962edb07e8e
Thomas Weise <tweise@hfuu.edu.cn>
2025-05-23 15:27:27 +0800

    hopefully fixed error

--- make.sh
+++ make.sh
@@ -8,4 +8,4 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   # set -u : exit the script if you try to use an uninitialized variable
 set -o errexit   # set -e : exit the script if any statement returns a non-true return value
 
-./bookbase/scripts/slidesMake.sh "Programming with Python" "https://thomasweise.github.io/programmingWithPythonSlides"
+./bookbase/scripts/slidesMake.sh "Programming with Python" "https://thomasweise.github.io/programmingWithPythonSlidesDE"
