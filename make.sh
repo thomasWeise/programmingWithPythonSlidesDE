476942f63496647435a2c6daa8b750deba72d36d
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-28 18:09:39 +0800

    next attempt to shorten builds

--- make.sh
+++ make.sh
@@ -8,4 +8,5 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   # set -u : exit the script if you try to use an uninitialized variable
 set -o errexit   # set -e : exit the script if any statement returns a non-true return value
 
+export NO_PDF_OPT  # Make builds shorter.
 ./bookbase/scripts/slidesMake.sh "Programming with Python" "https://thomasweise.github.io/programmingWithPythonSlidesDE"
